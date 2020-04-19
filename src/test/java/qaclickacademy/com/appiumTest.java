12d1b9aa6a63b8f31ba6bfe5bf72997118535f4d
rajshekar <rajshekar.lp@gmail.com>
2020-04-19 18:07:02 +0530

    Architect develop

--- src/test/java/qaclickacademy/com/appiumTest.java
+++ src/test/java/qaclickacademy/com/appiumTest.java
@@ -8,11 +8,20 @@ public class appiumTest {
 	public void nativeAppAndroid()
 	{
 		System.out.println("nativeAppAndroid");
+		System.out.println("nativeAppAndroid");
 	}
 	
 	@Test
+	public void nativeAppAndroid111()
+	{
+		System.out.println("nativeAppAndroid111");
+		System.out.println("nativeAppAndroid111");
+	}
+	@Test
 	public void iosApps()
 	{
+		System.out.println("iosApps");
+		System.out.println("iosApps");
 		System.out.println("iosApps");
 	}
 }
